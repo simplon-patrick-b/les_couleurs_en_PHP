c6fa80046596fadd017ff0fc3ce6f991312f1e40
patrickb <patrick.belaire@gmail.com>
2015-11-13 14:01:51 +0100

    premier

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$couleurs = array
+(
+//on defini les codes relies aux couleurs
+  'or' =>'#FFD700' ,
+  'turquoise' => '#00CED1' ,
+  'chocolat' => '#D2691E' ,
+  'tomate' => '#FF6347' ,
+  'argent' => "#C0C0C0" ,
+  'saumon' => "#FA8072"
+);
+//on demarre la boucle
+foreach ($couleurs as $cle => $valeur)
+{
+  //on affiche la cle et la couleur de la valeur de la table
+  echo "<button><strong><FONT size=\"16\" color=\"".$valeur."\">".$cle."</FONT></strong></button><br/><br/>";
+}
+ ?>
